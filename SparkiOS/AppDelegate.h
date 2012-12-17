2f6a1c0f328c1d636673f9bde38b7e3bc6d47de7
David Ragones <dragones@dreamcommerce.co>
2012-12-17 13:29:14 -0800

    [Delivers #41353409] create SparkiOS Xcode project

---
+++ SparkiOS/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  SparkiOS
+//
+//  Created by David Ragones on 12/17/12.
+//  Copyright (c) 2012 Financial Business Systems, Inc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ SparkiOS/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  SparkiOS
+//
+//  Created by David Ragones on 12/17/12.
+//  Copyright (c) 2012 Financial Business Systems, Inc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
